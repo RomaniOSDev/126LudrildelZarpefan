67d3cd31e76954c5025d28359f02741ab5fc28bc
Roman <romazan.26@gmail.com>
2026-03-30 16:17:35 +0300

    final

---
+++ 126LudrildelZarpefan/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  126LudrildelZarpefan
+//
+//  Created by Роман Главацкий on 30.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
